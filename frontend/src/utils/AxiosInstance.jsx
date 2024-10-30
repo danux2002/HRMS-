6e0ee5d3ca83119e7142b11a260b251f6558b3d9
Dineth Danurdha <163334443+danux2002@users.noreply.github.com>
2024-10-30 23:03:58 +0530

    init4

--- frontend/src/utils/AxiosInstance.jsx
+++ frontend/src/utils/AxiosInstance.jsx
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const axiosInstance = axios.create({
-  baseURL: `https://${process.env.HOST}:${process.env.PORT}/api/`,
+  baseURL: `https://${process.env.HOST}/api/`,
 });
 
 // Add a request interceptor to include the token in every request
